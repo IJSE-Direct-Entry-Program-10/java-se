5db247062b7e3b9624fb6a3dcf4255bf912eb75b
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-02-23 16:05:37 +0530

    Finish the copy app - II

--- file-io/file-copying-app/src/main/java/lk/ijse/dep10/copy/controller/CopySceneController.java
+++ file-io/file-copying-app/src/main/java/lk/ijse/dep10/copy/controller/CopySceneController.java
@@ -1,15 +1,17 @@
 package lk.ijse.dep10.copy.controller;
 
-import javafx.application.Platform;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Optional;
 
 public class CopySceneController {
@@ -33,7 +35,7 @@ public class CopySceneController {
 
     public void btnCopyOnAction(ActionEvent event) {
         File targetFile = new File(targetFolder, sourceFile.getName());
-        if(targetFile.exists()){
+        if (targetFile.exists()) {
             Optional<ButtonType> optResult = new Alert(Alert.AlertType.CONFIRMATION,
                     "File already exists, are you sure to replace the file?",
                     ButtonType.YES, ButtonType.NO).showAndWait();
@@ -41,34 +43,34 @@ public class CopySceneController {
         }
         btnCopy.getScene().getWindow().setHeight(325);
 
-        Task task = new Task<Void>(){
+        Task task = new Task<Void>() {
 
             @Override
             protected Void call() throws Exception {
-                try {
-                    FileInputStream fis = new FileInputStream(sourceFile);
-                    FileOutputStream fos = new FileOutputStream(targetFile);
+                FileInputStream fis = new FileInputStream(sourceFile);
+                FileOutputStream fos = new FileOutputStream(targetFile);
 
-                    double write = 0.0;
+                double write = 0.0;
 
-                    while (true) {
-                        byte[] buffer = new byte[1024 * 10];    // 10Kb
-                        int read = fis.read(buffer);
-                        if (read == -1) break;
-                        fos.write(buffer, 0, read);
-                        write += read;
-                        updateMessage(String.format("%2.2f", write / sourceFile.length() * 100).concat("% Complete"));
-                        updateProgress(write, sourceFile.length());
-                    }
-                    fis.close();
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                while (true) {
+                    byte[] buffer = new byte[1024 * 10];    // 10Kb
+                    int read = fis.read(buffer);
+                    if (read == -1) break;
+                    fos.write(buffer, 0, read);
+                    write += read;
+                    updateMessage(String.format("%2.2f", write / sourceFile.length() * 100).concat("% Complete"));
+                    updateProgress(write, sourceFile.length());
                 }
+                fis.close();
+                fos.close();
                 return null;
             }
         };
 
+        task.exceptionProperty().addListener(observable -> {
+            task.getException().printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Something went wrong, please try again!").show();
+        });
         lblStatus.textProperty().bind(task.messageProperty());
         prgCopy.progressProperty().bind(task.progressProperty());
         new Thread(task).start();
